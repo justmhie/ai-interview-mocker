246a691b310727aa86170a66ffd016c486173939
justmhie <alyssamhiee@gmail.com>
2024-08-06 15:27:13 +0800

    header

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,10 +1,25 @@
+"use client"
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 function Header() {
+  const path=usePathname();
+  useEffect(()=>{
+    console.log(path)
+  },[])
+
   return (
-    <div>
+    <div className='flex p-4 items-center justify-between bg-secondary shadow-md'>
       <Image src={'/logo.svg'} width={160} height={100} alt='logo'/>
+      <ul className='hidden md:flex gap-6'>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard'&&'text-primary font-bold'}`}>Dashboard</li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions'&&'text-primary font-bold'}`}>Questions</li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade'&&'text-primary font-bold'}`}>Uprade</li>
+        <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how'&&'text-primary font-bold'}`}>How it Works?</li>
+      </ul>
+      <UserButton/>
     </div>
   )
 }
